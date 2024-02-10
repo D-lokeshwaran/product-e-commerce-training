556d0472a0246c80e5081c445b4c6445cab0eb14
lenin <lenin.d402119019@gmail.com>
2024-02-10 12:23:48 +0530

    initial header changes

--- src/ProductECommerceTraining.jsx
+++ src/ProductECommerceTraining.jsx
@@ -5,16 +5,6 @@ class ProductECommerceApp extends React.Component {
     render() {
         return (
             <div class="product-e-commerce-app">
-                <header style={{padding: '1rem'}}>
-                    <h2>Hello! Welcome to E-Commerce training Project.</h2>
-                    <br/>
-                    <p style={{maxWidth: '500px'}}>
-                        you can <strong>customize code as per your need</strong> to complete this
-                        entire application make sure u use third party libraries effectively and
-                        <b> understand how they works</b>.
-                    </p><br/>
-                    <code>Remove this welcome header text and start building this app.</code>
-                </header>
                 {/* your app goes here */}
             </div>
         )
